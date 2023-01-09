5be00746f3d88ab3d5d55fc550194869205b104a
jeremiahlee94 <jhwlee94@gmail.com>
2023-01-09 14:51:54 -0800

    fix secret key issue

--- tax_django/settings.py
+++ tax_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-96pn)p165&mvjd2*j*5lp0%yk2d!jz1n^%&3@$*9*xtt+o088@'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
